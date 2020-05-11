2d8b846a9c28568c3277e494ba4b327140a8f9bc
Shubham Paul <paulplusx@gmail.com>
2020-05-11 16:17:57 +0200

    Update memoryop_2.s

--- software/ARM/Assembly/memoryop_2/src/memoryop_2.s
+++ software/ARM/Assembly/memoryop_2/src/memoryop_2.s
@@ -7,18 +7,18 @@
 
 _start:
         adr r5, dat_in
-		adr r6, dat_out
-		mov r3, #4
-		mov r2, #0
+	adr r6, dat_out
+	mov r3, #4
+	mov r2, #0
 		
 loop:
-		ldr r4, [r5], #4
-		ldr r1, [r5], #4
-		sub r3, r3, #1
-		add r0, r4, r1
-		str r0, [r6], #4
-		cmp r2, r3
-		bne loop
+	ldr r4, [r5], #4
+	ldr r1, [r5], #4
+	sub r3, r3, #1
+	add r0, r4, r1
+	str r0, [r6], #4
+	cmp r2, r3
+	bne loop
 
 
 dat_in: .word 1, 2, 3, 4, 5, 6, 7, 8
